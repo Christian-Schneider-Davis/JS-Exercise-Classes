5145d9c74e746de95fc5d4b9da6437e3cd80ab5b
Christian Schneider-Davis <75326583+Christian-Schneider-Davis@users.noreply.github.com>
2020-12-17 20:14:49 -0600

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 /*
-  TASK:
+   TASK:
     - Write an Airplane class whose constructor initializes `name` from an argument.
     - All airplanes built with Airplane should initialize with an `isFlying` property of false.
     - Give airplanes the ability to `.takeOff()` and `.land()`:
@@ -8,7 +8,7 @@
         + If a plane lands, its `isFlying` property gets set to false.
 */
 
-//  SOLUTION CODE:
+// SOLUTION CODE:
 class Airplane {
     constructor(name) {
       this.name = name;
